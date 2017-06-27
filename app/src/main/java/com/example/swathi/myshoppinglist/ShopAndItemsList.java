ff2047cf7378eb11cb4ed285825b4c0200c53f6a
swathirs <rswathi@pdx.edu>
2017-06-27 11:39:04 -0700

    initial commit

---
+++ app/src/main/java/com/example/swathi/myshoppinglist/ShopAndItemsList.java
@@ -0,0 +1,18 @@
+package com.example.swathi.myshoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopAndItemsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_and_items_list);
+    }
+    void testMethod()
+    {
+        int i  = 0;
+        i = i+1;
+    }
+}
